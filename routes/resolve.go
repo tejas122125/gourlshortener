ce2d9e3758b15401aac6328c5ba45c7f6ce688d6
tejas122125 <tejasweekumarsingh@gmail.com>
2024-09-03 00:26:40 +0530

    tejasd

--- routes/resolve.go
+++ routes/resolve.go
@@ -1,5 +1,23 @@
-package routes 
+package routes
 
-func ResolveUrl () {
-	
+import (
+	"urlshortener/database"
+
+	"github.com/go-redis/redis/v8"
+	"github.com/gofiber/fiber/v2"
+)
+
+
+
+func ResolveUrl(c *fiber.Ctx) error {
+
+url := c.Params("url")
+
+ r := database.CreateClient(0)
+
+ defer r.Close()
+
+ r.Get(database.ctx)
+
+
 }
